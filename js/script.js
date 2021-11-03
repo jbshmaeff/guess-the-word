132a000dc65ba47012c047518347447219325637
Joselyn_Shmaeff <jbshmaeff@yahoo.com>
2021-11-03 14:59:41 -0700

    Step 1 of word game complete

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// The unordered list where the player’s guessed letters will appear.
+const guessedLetters = document.querySelector(".guessed-letters");
+
+// The button with the text “Guess!” in it.
+const guessButton = document.querySelector(".guess");
+
+// The text input where the player will guess a letter.
+const letterInput = document.querySelector(".letter");
+
+// The empty paragraph where the word in progress will appear.
+const wordInProgress = document.querySelector(".word-in-progress");
+
+// The paragraph where the remaining guesses will display.
+const guessesRemaining = document.querySelector(".remaining");
+
+// The span inside the paragraph where the remaining guesses will display.
+const span = document.querySelector(".remaining span");
+
+// The empty paragraph where messages will appear when the player guesses a letter.
+const message = document.querySelector("message");
+
+// The hidden button that will appear prompting the player to play again.
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+//Write a function to add placeholders for each letter
+
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join ("");
+};
+placeholder(word);
+
+//Add an event listener for the button
+
+guessButton.addEventListener ("click", function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+})
